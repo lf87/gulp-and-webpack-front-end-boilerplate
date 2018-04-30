743a219c0e4b88ea0a1fb2a90c1ff52f97c0f7b1
Luke Fryer <luke.fryer@epiphany.co.uk>
2018-04-30 13:48:25 +0100

    Added browser sync callback

--- config/tasks/watch.js
+++ config/tasks/watch.js
@@ -8,15 +8,16 @@ export function watch() {
   gulp.watch(Config.src.pages, gulp.series('nunjucksPages'))
   gulp.watch(Config.src.templates, gulp.series('nunjucksTemplates'))
   gulp.watch(Config.config.pagesWatch, gulp.series(htmlInjector))
-  gulp.watch(Config.src.scss, gulp.series('sass'))
-  gulp.watch(Config.src.js, gulp.series('webpackBundle'))
-  gulp.watch(Config.src.img, gulp.series('images'))
-  gulp.watch(Config.src.imgPng, gulp.series('imagesPng'))
-  gulp.watch(Config.src.svg, gulp.series('svgs'))
-  gulp.watch(Config.src.fonts, gulp.series('fonts'))
-  gulp.watch(Config.src.docs, gulp.series('duplicateFiles'))
+  gulp.watch(Config.src.scss, gulp.series('sass', 'bsReload'))
+  gulp.watch(Config.src.webpack, gulp.series('webpackBundle', 'bsReload'))
+  gulp.watch(Config.src.img, gulp.series('images', 'bsReload'))
+  gulp.watch(Config.src.imgPng, gulp.series('imagesPng', 'bsReload'))
+  gulp.watch(Config.src.svg, gulp.series('svgs', 'bsReload'))
+  gulp.watch(Config.src.fonts, gulp.series('fonts', 'bsReload'))
+  gulp.watch(Config.src.docs, gulp.series('duplicateFiles', 'bsReload'))
 }
 
-// module.exports = {
-//   watch
-// }
+module.exports = {
+  watch
+}
+
