5dac363905541bf734bed7729ffe3fb79f3affab
Luke Fryer <luke.fryer@epiphany.co.uk>
2018-04-30 13:47:48 +0100

    Added task functionality back in

--- config/tasks/clean.js
+++ config/tasks/clean.js
@@ -5,17 +5,18 @@ import notify from 'gulp-notify' // Notifications upon task completion
 const Config = require('../config')
 
 export function clean() {
-  // del([
-  //   `${Config.dist.pages}*${Config.fileExt}`,
-  //   `${Config.dist.css}/*.css`,
-  //   Config.dist.js,
-  //   Config.dist.img,
-  //   Config.dist.fonts,
-  //   Config.dist.docs,
-  //   Config.dist.favicons,
-  //   Config.config.maps,
-  //   Config.config.reports
-  // ])
+  del([
+    `${Config.dist.pages}*${Config.fileExt}`,
+    `${Config.dist.css}/*.css`,
+    `${Config.dest}`,
+    `${Config.dist.webpack}`,
+    `${Config.dist.img}`,
+    `${Config.dist.fonts}`,
+    `${Config.dist.docs}`,
+    `${Config.dist.favicons}`,
+    `${Config.config.maps}`,
+    `${Config.config.reports}`
+  ])
   return gulp.src('./').pipe(
     notify({
       message: 'Folders cleaned successfully',
