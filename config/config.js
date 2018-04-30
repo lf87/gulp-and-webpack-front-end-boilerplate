51b1dac34dd25acc04226f15de1fb9d1c712fe26
Luke Fryer <luke.fryer@epiphany.co.uk>
2018-04-30 14:16:43 +0100

    Updated comments and made more dynamic

--- config/config.js
+++ config/config.js
@@ -6,14 +6,13 @@ if (notifications) {
   process.env.DISABLE_NOTIFIER = true // Uncomment to disables all notifications
 }
 
-// Set source and distribution folders
+// Paths
+const fileExt = '.html' // Default file extension
 const source = 'src'
 const dest = 'dist'
+const root = 'assets'
+const assets = 'assets'
 
-// Set file extension
-const fileExt = '.html'
-
-// Paths
 const src = {
   pages: `${source}/pages/*${fileExt}`,
   templates: `${source}/templates/**/*${fileExt}`,
@@ -28,24 +27,22 @@ const src = {
 }
 
 const dist = {
-  pages: './',
-  css: './',
-  webpack: `${dest}/assets/js`,
-  img: `${dest}/assets/img`,
-  svg: `${dest}/assets/img/svg`,
-  fonts: `${dest}/assets/fonts`,
-  docs: `${dest}/assets/docs`,
-  favicons: `${dest}/assets/favicon`
+  pages: `${root}`,
+  css: `${root}`,
+  webpack: `${dest}${assets}/js`,
+  img: `${dest}${assets}/img`,
+  svg: `${dest}${assets}/img/svg`,
+  fonts: `${dest}${assets}/fonts`,
 }
 
 const config = {
-  maps: 'maps', // This is where your CSS and JS sourcemaps go
-  reports: 'reports', // Lint reports go here
+  maps: 'maps', // This is where your CSS sourcemaps are saved
+  reports: 'reports', // Lint reports saved here
   lint: `${source}/styles/**/*.scss`, // Path of SCSS files that you want to lint
   lintExclude: `!${source}/styles/vendor/**/*.scss`, // Path of SCSS files that you want to exclude from lint
-  templates: [`${source}/templates/`, `${source}/templates/partials/`],
-  pagesWatch: `./*${fileExt}`, // Directory where pages are output
-  production: !!util.env.production, // Used for prod deployment
+  templates: [`${source}/templates/`, `${source}/templates/partials/`], // Default file paths for nunjucks
+  pagesWatch: `${root}*${fileExt}`, // HTML injection  - This is the directory where pages are output
+  production: !!util.env.production, // Used for production deployment
   criticalCss: dist.css + '/style.css' // Accepts arrays e.g. [dist.css + '/components.css', dist.css + '/main.css']
 }
 
