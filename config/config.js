ca955c39ef72b2988185d6579516e581b035cb5a
Luke Fryer <luke.fryer@epiphany.co.uk>
2018-04-30 13:47:26 +0100

    Tidy

--- config/config.js
+++ config/config.js
@@ -1,20 +1,19 @@
+import util from 'gulp-util'
+
 // Disable or enable pop up notifications
 const notifications = false
 if (notifications) {
   process.env.DISABLE_NOTIFIER = true // Uncomment to disables all notifications
 }
 
-// const browserSync = require('browser-sync').create() // Create BS server
-// const critical = require('critical').stream // Inlines above the fold CSS
-
+// Set source and distribution folders
 const source = 'src'
 const dest = 'dist'
 
-// // File Format
-const fileFormat = 'html'
-const fileExt = `.${fileFormat}`
+// Set file extension
+const fileExt = '.html'
 
-// Paths object
+// Paths
 const src = {
   pages: `${source}/pages/*${fileExt}`,
   templates: `${source}/templates/**/*${fileExt}`,
@@ -45,15 +44,15 @@ const config = {
   lint: `${source}/styles/**/*.scss`, // Path of SCSS files that you want to lint
   lintExclude: `!${source}/styles/vendor/**/*.scss`, // Path of SCSS files that you want to exclude from lint
   templates: [`${source}/templates/`, `${source}/templates/partials/`],
-  pagesWatch: `./*${fileExt}` // Directory where pages are output
-  // production: !!util.env.production, // Used for prod deployment
-  // criticalCss: dist.css + '/style.css' // Accepts arrays e.g. [dist.css + '/components.css', dist.css + '/main.css']
+  pagesWatch: `./*${fileExt}`, // Directory where pages are output
+  production: !!util.env.production, // Used for prod deployment
+  criticalCss: dist.css + '/style.css' // Accepts arrays e.g. [dist.css + '/components.css', dist.css + '/main.css']
 }
 
 module.exports = {
-  // dir,
   source,
   dest,
+  fileExt,
   src,
   dist,
   config
