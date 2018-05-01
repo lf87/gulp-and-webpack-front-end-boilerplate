9313b9acfd6ef60ea064d3e5efdb455f892d53d7
Luke Fryer <luke.fryer@epiphany.co.uk>
2018-05-01 09:37:14 +0100

    Simplified JS import example

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,10 +2,5 @@
  * @title App
  * @description Application entry point
  */
-import {Cat, Dog} from './partial'
+// import {Something} from './partial'
 
-const myCat = new Cat('Spike')
-console.log('Cat name:', myCat.meow())
-
-const myDog = new Dog('Stanley', 'Labrador')
-console.log('Dog name and breed:', myDog.bark())
--- src/scripts/partial.js
+++ src/scripts/partial.js
@@ -1,19 +1,2 @@
 // partial.js
-export class Cat {
-  constructor(name) {
-    this.name = name
-  }
-  meow() {
-    return `${this.name}`
-  }
-}
-
-export class Dog {
-  constructor(name, breed) {
-    this.name = name
-    this.breed = breed
-  }
-  bark() {
-    return `${this.name} is a ${this.breed}`
-  }
-}
+export class Something {}
