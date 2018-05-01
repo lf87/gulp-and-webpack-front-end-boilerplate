6d73212be99868700375d70f1426255368c3702b
Luke Fryer <luke.fryer@epiphany.co.uk>
2018-05-01 09:06:18 +0100

    HTML and CSS defaults to dist folder instead of root

--- config/config.js
+++ config/config.js
@@ -9,8 +9,8 @@ if (notifications) {
 // Paths
 const fileExt = '.html' // Default file extension
 const source = 'src'
+const root = './'
 const dest = 'dist'
-const root = 'assets'
 const assets = 'assets'
 
 const src = {
@@ -18,26 +18,26 @@ const src = {
   templates: `${source}/templates/**/*${fileExt}`,
   scss: `${source}/styles/**/*.scss`,
   webpack: `${source}/scripts/**/*.js`,
-  img: `${source}/images/**/*.{jpg,gif}`,
-  imgPng: `${source}/images/**/*.png`,
-  svg: `${source}/images/svgs/**/*.svg`,
+  img: `${source}/img/**/*.{jpg,gif}`,
+  imgPng: `${source}/img/**/*.png`,
+  svg: `${source}/img/svgs/**/*.svg`,
   fonts: `${source}/fonts/**/*`,
-  docs: `${source}/docs/**/*`,
-  favicons: `${source}/favicons/**/*`
+  static: `${source}/static/**/*`,
 }
 
 const dist = {
-  pages: `${root}`,
-  css: `${root}`,
-  webpack: `${dest}${assets}/js`,
-  img: `${dest}${assets}/img`,
-  svg: `${dest}${assets}/img/svg`,
-  fonts: `${dest}${assets}/fonts`,
+  pages: `${root}${dest}`,
+  css: `${root}${dest}`,
+  static: `${dest}/${assets}`,
+  webpack: `${dest}/${assets}/js`,
+  img: `${dest}/${assets}/img`,
+  svg: `${dest}/${assets}/img/svgs`,
+  fonts: `${dest}/${assets}/fonts`,
 }
 
 const config = {
-  maps: 'maps', // This is where your CSS sourcemaps are saved
-  reports: 'reports', // Lint reports saved here
+  maps: '../maps', // This is where your CSS sourcemaps are saved
+  reports: '../reports', // Lint reports saved here
   lint: `${source}/styles/**/*.scss`, // Path of SCSS files that you want to lint
   lintExclude: `!${source}/styles/vendor/**/*.scss`, // Path of SCSS files that you want to exclude from lint
   templates: [`${source}/templates/`, `${source}/templates/partials/`], // Default file paths for nunjucks
@@ -49,8 +49,9 @@ const config = {
 module.exports = {
   source,
   dest,
+  assets,
   fileExt,
   src,
   dist,
-  config
+  config,
 }
