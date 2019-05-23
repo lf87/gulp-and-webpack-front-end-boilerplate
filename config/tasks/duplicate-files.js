cb5285d806fc6d910719550c4f2298613bc90f2e
Luke Fryer <luke.fryer@epiphany.co.uk>
2019-05-23 08:55:20 +0100

    Duplicate files task updated

--- config/tasks/duplicate-files.js
+++ config/tasks/duplicate-files.js
@@ -1,20 +1,25 @@
 import gulp from 'gulp'
 import changed from 'gulp-changed'
+import notify from 'gulp-notify'
 
 const Config = require('../config')
 
-export function duplicateFiles() {
+export function duplicateFiles () {
   return gulp.src(Config.src.static, {
-        allowEmpty: true,
-        nodir: true
-      })
-      .pipe(changed(Config.dist.static, {
-        hasChanged: changed.compareLastModifiedTime
-      }))
-  .pipe(gulp.dest(Config.dist.static))
+    allowEmpty: true,
+    nodir: true
+  })
+    .pipe(changed(Config.dist.static, {
+      hasChanged: changed.compareLastModifiedTime
+    }))
+    .pipe(gulp.dest(Config.dist.static))
 }
 
-export function duplicateProdFiles() {
-  return gulp.src(`${Config.dest}/**/*`)
-  .pipe(gulp.dest(Config.dist.production))
+export function duplicateProdFiles () {
+  return gulp.src(`${Config.dest}/**/*`, {
+    allowEmpty: true,
+    nodir: true
+  })
+    .pipe(gulp.dest(Config.dist.production))
+    .pipe(notify({ message: `The assets have been copied to ${Config.dist.production}`, onLast: true }))
 }
