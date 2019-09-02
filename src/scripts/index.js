f466af447258ee611da50d53460a218070fc16e2
Luke Fryer <luke.fryer@epiphany.co.uk>
2019-09-02 14:51:35 +0100

    Added comment to js with regard to sw.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,5 +5,6 @@
 import ServiceWorker from './ServiceWorker'
 
 window.addEventListener('load', function () {
+  // If removing the service worker, then don't forget to remove from the webpack config as well
   new ServiceWorker()
 })
